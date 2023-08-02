c02aabfdfe533447ebd3ede23b4414bacfb4fded
gksehfapd <1ehfapd@gmail.com>
2023-08-03 00:20:54 +0900

    test: useForm - validation

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,13 +1,54 @@
-import { useForm } from 'react-hook-form'
+import { FieldErrors, useForm } from 'react-hook-form'
+
+interface LoginForm {
+	username: string
+	password: string
+	email: string
+}
 
 export default function Forms() {
-	const { register } = useForm()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<LoginForm>({ mode: 'onBlur' })
+
+	const onValid = (data: LoginForm) => {
+		console.log('hi')
+	}
+
+	const onInValid = (errors: FieldErrors) => {
+		console.log(errors)
+	}
 
 	return (
-		<form>
-			<input {...register('username')} type="text" placeholder="Username" required />
-			<input {...register('email')} type="email" placeholder="Email" required />
-			<input {...register('password')} type="password" placeholder="Password" required />
+		<form onSubmit={handleSubmit(onValid, onInValid)}>
+			<input
+				{...register('username', {
+					required: 'Username is Required',
+					minLength: { message: 'more then 6', value: 6 }
+				})}
+				type="text"
+				placeholder="Username"
+			/>
+
+			<input
+				{...register('email', {
+					required: 'Email is Required',
+					validate: {
+						notGmail: (value) =>
+							!value.includes('@gmail.com') ? '' : 'Gmail is not allowed'
+					}
+				})}
+				type="email"
+				placeholder="Email"
+			/>
+			{errors.email?.message}
+			<input
+				{...register('password', { required: 'PW is Required' })}
+				type="password"
+				placeholder="Password"
+			/>
 			<input type="submit" value="Create Account" />
 		</form>
 	)
