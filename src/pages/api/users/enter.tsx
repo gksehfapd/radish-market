9453ffaaedd7641e3e3745c3a459171dd1ce6f15
gksehfapd <1ehfapd@gmail.com>
2023-08-03 17:37:55 +0900

    feat: Token Logic

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -2,56 +2,27 @@ import client from '@/libs/server/client'
 import withHandler from '@/libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { phone, email } = req.body
-	const payload = phone ? { phone: +phone } : { email }
+interface ResponseType {
+	ok: boolean
+	[key: string]: any
+}
 
-	const user = await client.user.upsert({
-		where: { ...payload },
-		create: { name: 'Anonymous', ...payload },
-		update: {}
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+	const { phone, email } = req.body
+	const user = phone ? { phone: +phone } : { email }
+	const payload = Math.floor(100000 + Math.random() * 900000) + ''
+	const token = await client.token.create({
+		data: {
+			payload,
+			user: {
+				connectOrCreate: {
+					where: { ...user },
+					create: { name: 'Anonymous', ...user }
+				}
+			}
+		}
 	})
-	console.log(user)
-	// if (email) {
-	// 	user = await client.user.findUnique({
-	// 		where: {
-	// 			email
-	// 		}
-	// 	})
-	// 	if (user) {
-	// 		console.log('founded')
-	// 	}
-	// 	if (!user) {
-	// 		console.log('did not find. will create')
-	// 		user = await client.user.create({
-	// 			data: {
-	// 				name: 'Anonymous',
-	// 				email
-	// 			}
-	// 		})
-	// 	}
-	// 	console.log(user)
-	// }
-	// if (phone) {
-	// 	user = await client.user.findUnique({
-	// 		where: {
-	// 			phone: +phone
-	// 		}
-	// 	})
-	// 	if (user) {
-	// 		console.log('founded')
-	// 	}
-	// 	if (!user) {
-	// 		console.log('did not find. will create')
-	// 		user = await client.user.create({
-	// 			data: {
-	// 				name: 'Anonymous',
-	// 				phone: +phone
-	// 			}
-	// 		})
-	// 	}
-	// 	console.log(user)
-	// }
+
 	res.status(200).end()
 }
 
