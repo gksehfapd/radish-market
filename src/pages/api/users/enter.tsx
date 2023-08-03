63ad754806c54d24a521db31a433594300bfd476
gksehfapd <1ehfapd@gmail.com>
2023-08-03 21:02:48 +0900

    feat: Sending SMS

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,15 +1,15 @@
 import client from '@/libs/server/client'
-import withHandler from '@/libs/server/withHandler'
+import withHandler, { ResponseType } from '@/libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
+import Twilio from 'twilio'
 
-interface ResponseType {
-	ok: boolean
-	[key: string]: any
-}
+const twilioClient = Twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
 	const { phone, email } = req.body
-	const user = phone ? { phone: +phone } : { email }
+	const user = phone ? { phone: +phone } : email ? { email } : null
+	if (!user) return res.status(400).json({ ok: false })
+
 	const payload = Math.floor(100000 + Math.random() * 900000) + ''
 	const token = await client.token.create({
 		data: {
@@ -22,8 +22,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 			}
 		}
 	})
-
-	res.status(200).end()
+	if (phone) {
+		const message = await twilioClient.messages.create({
+			messagingServiceSid: process.env.TWILIO_MSID,
+			to: process.env.MY_PHONE!,
+			body: `Your login token is ${payload}.`
+		})
+		console.log(message)
+	}
+	return res.json({ ok: true })
 }
 
 export default withHandler('POST', handler)
