65a457a6ba7744e77916d37f10761dbd2901a30c
gksehfapd <1ehfapd@gmail.com>
2023-08-05 21:37:54 +0900

    feat: Fav Products

--- src/pages/api/products/[id]/index.ts
+++ src/pages/api/products/[id]/index.ts
@@ -4,7 +4,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { withApiSession } from '@/libs/server/withSession'
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-	const { id } = req.query
+	const {
+		query: { id },
+		session: { user }
+	} = req
 
 	const product = await client.product.findUnique({
 		where: {
@@ -20,11 +23,13 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 			}
 		}
 	})
+
 	const terms = product?.name.split(' ').map((word) => ({
 		name: {
 			contains: word
 		}
 	}))
+
 	const relatedProducts = await client.product.findMany({
 		where: {
 			OR: terms,
@@ -36,8 +41,19 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 		}
 	})
 
-	// console.log(relatedProducts)
-	res.json({ ok: true, product, relatedProducts })
+	const isLiked = Boolean(
+		await client.fav.findFirst({
+			where: {
+				productId: product?.id,
+				userId: user?.id
+			},
+			select: {
+				id: true
+			}
+		})
+	)
+
+	res.json({ ok: true, product, isLiked, relatedProducts })
 }
 
 export default withApiSession(
