7a9c0dfde9b905135633949f33e78a80ecd454bc
gksehfapd <1ehfapd@gmail.com>
2023-08-19 22:38:23 +0900

    feat: middleware protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
+import { NextResponse, userAgent } from 'next/server'
 import type { NextRequest, NextFetchEvent } from 'next/server'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-	console.log('hello middleware')
-
 	if (req.nextUrl.pathname.startsWith('/chats')) {
-		console.log('only chats')
+		console.log('chats Only middleware')
 	}
+	if (req.nextUrl.pathname.startsWith('/')) {
+		const ua = userAgent(req)
+		if (ua?.isBot) {
+			return new Response("Plz don't be a bot. Be human.", { status: 403 })
+		}
+	}
+	if (req.nextUrl.pathname.startsWith('/api')) {
+		if (!req.url.includes('/enter') && !req.cookies.get('radishsession')) {
+			console.log('radishsession')
+			NextResponse.redirect(`${req.nextUrl.origin}/enter`)
+		}
+	}
+
+	return NextResponse.next()
 }
