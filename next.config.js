5f927c7e646b67ed2a8009dc8242bdf73c32c9f8
gksehfapd <1ehfapd@gmail.com>
2023-09-10 23:16:48 +0900

    recap : reset radish-market

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ['imagedelivery.net', 'videodelivery.net']
-	}
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
