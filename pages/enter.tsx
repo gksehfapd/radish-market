2cff655bae4f1c86f25735871d0ab76e5d751122
gksehfapd <1ehfapd@gmail.com>
2023-09-16 17:53:43 +0900

    recap : useMutation hook

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import Button from '../components/button'
-import Input from '../components/input'
-import { cls } from '../libs/utils'
+import Button from '@/components/button'
+import Input from '@/components/input'
+import { cls } from '@/libs/client/utils'
 import { useForm } from 'react-hook-form'
+import useMutation from '@/libs/client/useMutation'
 
 interface EnterForm {
 	email?: string
@@ -11,6 +12,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+	const [enter, { loading, data, error }] = useMutation('/api/users/enter')
 	const [submitting, setSubmitting] = useState(false)
 	const { register, reset, handleSubmit } = useForm<EnterForm>()
 	const [method, setMethod] = useState<'email' | 'phone'>('email')
@@ -23,14 +25,10 @@ const Enter: NextPage = () => {
 		setMethod('phone')
 	}
 
-	const onValid = (data: EnterForm) => {
-		setSubmitting(true)
-		fetch('/api/users/enter', {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: { 'Content-type': 'application/json' }
-		}).then(() => setSubmitting(false))
+	const onValid = (validForm: EnterForm) => {
+		enter(validForm)
 	}
+	console.log(loading, data, error)
 
 	return (
 		<div className="mt-16 px-4">
