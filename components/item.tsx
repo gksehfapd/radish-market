899a8b9d9d7bfdac450bd7c7850307d42c34cea4
gksehfapd <1ehfapd@gmail.com>
2023-09-16 16:23:21 +0900

    recap: enter form

--- components/item.tsx
+++ components/item.tsx
@@ -8,14 +8,27 @@ interface ItemProps {
 	hearts: number
 }
 
-export default function Item({ title, price, comments, hearts, id }: ItemProps) {
+export default function Item({
+	title,
+	price,
+	comments,
+	hearts,
+	id
+}: ItemProps) {
 	return (
-		<Link href={`/items/${id}`} className="flex px-4 pt-5 cursor-pointer justify-between">
+		<Link
+			href={`/items/${id}`}
+			className="flex px-4 pt-5 cursor-pointer justify-between"
+		>
 			<div className="flex space-x-4">
 				<div className="w-20 h-20 bg-gray-400 rounded-md" />
 				<div className="pt-2 flex flex-col">
-					<h3 className="text-sm font-medium text-gray-900">{title}</h3>
-					<span className="font-medium mt-1 text-gray-900">${price}</span>
+					<h3 className="text-sm font-medium text-gray-900">
+						{title}
+					</h3>
+					<span className="font-medium mt-1 text-gray-900">
+						${price}
+					</span>
 				</div>
 			</div>
 			<div className="flex space-x-2 items-end justify-end">
