ef44916158c72f25cdf9dad1156991a2e92dbc38
gksehfapd <1ehfapd@gmail.com>
2023-09-18 15:07:47 +0900

    recap: Severless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
 import client from '@/libs/server/client'
 import withHandler from '@/libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
@@ -6,9 +7,25 @@ import { ResponseType } from '@/libs/server/withHandler'
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
 	const { token } = req.body
 
-	console.log(token)
+	const exists = await client.token.findUnique({
+		where: {
+			payload: token
+		}
+	})
+
+	if (!exists) res.status(404).end()
+
+	req.session.user = {
+		id: exists?.userId
+	}
+
+	await req.session.save()
+	//logout 하려면 save() 를 destroy()로 변경
 
 	return res.status(200).end()
 }
 
-export default withHandler('POST', handler)
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+	cookieName: 'carrotsession',
+	password: `${process.env.COOKIE_PASSWORD}`
+})
