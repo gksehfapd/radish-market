14ca7df6975ea5804253393741b0a23ac6806e4e
gksehfapd <1ehfapd@gmail.com>
2023-09-26 23:31:51 +0900

    recap: Related Products

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -20,7 +20,25 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 			}
 		}
 	})
-	res.json({ ok: true, product })
+	const terms = product?.name.split(' ').map((word) => ({
+		name: {
+			contains: word
+		}
+	}))
+
+	const relatedProducts = await client.product.findMany({
+		where: {
+			OR: terms,
+			AND: {
+				id: {
+					not: product?.id
+				}
+			}
+		}
+	})
+
+	console.log(relatedProducts)
+	res.json({ ok: true, product, relatedProducts })
 }
 
 export default withApiSession(withHandler({ methods: ['GET'], handler }))
