1d1697cccfcb85cbf8fdedcf900561217a71ed81
gksehfapd <1ehfapd@gmail.com>
2023-10-03 23:21:32 +0900

    recap: All Posts

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -10,20 +10,46 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 		session: { user }
 	} = req
 
-	const post = await client.post.create({
-		data: {
-			question,
-			user: {
-				connect: {
-					id: user?.id
+	if (req.method === 'POST') {
+		const post = await client.post.create({
+			data: {
+				question,
+				user: {
+					connect: {
+						id: user?.id
+					}
 				}
 			}
-		}
-	})
-	return res.json({
-		ok: true,
-		post
-	})
+		})
+		return res.json({
+			ok: true,
+			post
+		})
+	}
+
+	if (req.method === 'GET') {
+		const posts = await client.post.findMany({
+			include: {
+				user: {
+					select: {
+						id: true,
+						name: true,
+						avatar: true
+					}
+				},
+				_count: {
+					select: {
+						Wondering: true,
+						answers: true
+					}
+				}
+			}
+		})
+		res.json({
+			ok: true,
+			posts
+		})
+	}
 }
 
-export default withApiSession(withHandler({ methods: ['POST'], handler }))
+export default withApiSession(withHandler({ methods: ['GET', 'POST'], handler }))
