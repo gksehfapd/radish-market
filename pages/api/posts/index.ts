4a2a1fb4eee035dff9d344e3b58ae949c7c02da6
gksehfapd <1ehfapd@gmail.com>
2023-10-04 23:33:09 +0900

    recap: Geo Search

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -5,12 +5,11 @@ import { ResponseType } from '@/libs/server/withHandler'
 import { withApiSession } from '@/libs/server/withSession'
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-	const {
-		body: { question, latitude, longitude },
-		session: { user }
-	} = req
-
 	if (req.method === 'POST') {
+		const {
+			body: { question, latitude, longitude },
+			session: { user }
+		} = req
 		const post = await client.post.create({
 			data: {
 				question,
@@ -30,23 +29,62 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
 	}
 
 	if (req.method === 'GET') {
-		const posts = await client.post.findMany({
-			include: {
-				user: {
-					select: {
-						id: true,
-						name: true,
-						avatar: true
+		const {
+			query: { latitude, longitude }
+		} = req
+
+		let posts
+
+		if (latitude && longitude) {
+			const parsedLatitude = parseFloat(latitude.toString())
+			const parsedLongitude = parseFloat(longitude.toString())
+
+			posts = await client.post.findMany({
+				include: {
+					user: {
+						select: {
+							id: true,
+							name: true,
+							avatar: true
+						}
+					},
+					_count: {
+						select: {
+							Wondering: true,
+							answers: true
+						}
 					}
 				},
-				_count: {
-					select: {
-						Wondering: true,
-						answers: true
+				where: {
+					latitude: {
+						gte: parsedLatitude - 0.01,
+						lte: parsedLatitude + 0.01
+					},
+					longitude: {
+						gte: parsedLongitude - 0.01,
+						lte: parsedLongitude + 0.01
 					}
 				}
-			}
-		})
+			})
+		} else {
+			posts = await client.post.findMany({
+				include: {
+					user: {
+						select: {
+							id: true,
+							name: true,
+							avatar: true
+						}
+					},
+					_count: {
+						select: {
+							Wondering: true,
+							answers: true
+						}
+					}
+				}
+			})
+		}
 		res.json({
 			ok: true,
 			posts
