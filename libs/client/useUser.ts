c48668f20252cd9e638d529753dbcdca4f5ccdb6
gksehfapd <1ehfapd@gmail.com>
2023-10-11 23:51:14 +0900

    recap: Edir Profile FE

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,11 +1,17 @@
+import { User } from '@prisma/client'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
+interface ProfileResponse {
+	ok: boolean
+	profile: User
+}
+
 export default function useUser() {
-	const { data, error } = useSWR('/api/users/me')
+	const { data, error } = useSWR<ProfileResponse>('/api/users/me')
 	const router = useRouter()
 	useEffect(() => {
 		if (data && !data.ok) {
